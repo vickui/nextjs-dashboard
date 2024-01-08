60c903c0537fac1f2756eca7d5d9861c71c53653
Victoria <vicky_864@hotmail.com>
2024-01-08 20:17:32 -0300

    Dashboard Foundation

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana, Outfit } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export const outfit = Outfit({ subsets: ['latin'] });
